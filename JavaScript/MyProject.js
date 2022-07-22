510f1f8799aa2361827055c54cfcf672791c5b01
agus alfandi <agusalfandi8@gmail.com>
2022-07-22 08:07:14 +0700

    day5

--- JavaScript/MyProject.js
+++ JavaScript/MyProject.js
@@ -23,43 +23,43 @@ function myBlog() {
   //   projekSaya.push(project);
   //   Project();
 }
-// function Project() {
-//   let projectWrapper = document.getElementById("contents");
+function Project() {
+  let projectWrapper = document.getElementById("contents");
 
-//   for (let i = 0; 0 < projekSaya.length; i++) {
-//     projectWrapper += `<div class="project-list-item">
-//     <a href="../Page/ProjectDetail.html">
-//         <div class="card-img">
-//             <img src=${projekSaya[i].image}>
-//         </div>
-//         <div class="card-title">
-//             <h3>${projekSaya[i].title}</h3>
-//         </div>
-//         <div class="card-drs">
-//             <p>Durasi 3 bulan</p>
-//             ${projekSaya[i].postAt} | ${projekSaya[i].author}
-//         </div>
-//         <div class="card-desc">
-//             <p>
-//                 ${projekSaya[i].content}
-//               </p>
-//         </div>
-//         <div class="card-icon">
-//             <img src="../Assets/img/javascript.png">
-//             <img src="../Assets/img/node.png">
-//             <img src="../Assets/img/react.png">
-//             <img src="../Assets/img/type.png">
-//         </div>
-//     </a>
-//     <div class="card-btn">
-//         <div class="edit">
-//             <button>edit</button>
-//         </div>
-//         <div class="card-delete">
-//             <button>delete</button>
-//         </div>
-//     </div>
+  for (let i = 0; 0 < projekSaya.length; i++) {
+    projectWrapper += `<div class="project-list-item">
+    <a href="../Page/ProjectDetail.html">
+        <div class="card-img">
+            <img src=${projekSaya[i].image}>
+        </div>
+        <div class="card-title">
+            <h3>${projekSaya[i].title}</h3>
+        </div>
+        <div class="card-drs">
+            <p>Durasi 3 bulan</p>
+            ${projekSaya[i].postAt} | ${projekSaya[i].author}
+        </div>
+        <div class="card-desc">
+            <p>
+                ${projekSaya[i].content}
+              </p>
+        </div>
+        <div class="card-icon">
+            <img src="../Assets/img/javascript.png">
+            <img src="../Assets/img/node.png">
+            <img src="../Assets/img/react.png">
+            <img src="../Assets/img/type.png">
+        </div>
+    </a>
+    <div class="card-btn">
+        <div class="edit">
+            <button>edit</button>
+        </div>
+        <div class="card-delete">
+            <button>delete</button>
+        </div>
+    </div>
 
-// </div>`;
-//   }
-// }
+</div>`;
+  }
+}
